3d526b265063eaec50292d3800cce02bccff57a2
gaurav-singh214 <singhgaurav97624@gmail.com>
2025-02-13 14:56:39 +0530

    Added untracked files

---
+++ array/simple.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using  namespace std;
+int main(){
+    int a,b;
+    cout<<"enter the no 1."<<endl;
+    cin>>a;
+    cout<<"enter the no 1."<<endl;
+    cin>>b;
+    int sum=a+b;
+    cout<<sum<<endl;
+
+    // cout<<"hello word"<<endl;
+}
